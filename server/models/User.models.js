1ad58fc306a08be72284ee89ecb722a8d5c670be
Krishna-sm <krishna.web18@gmail.com>
2023-09-18 19:28:50 +0530

    password hasing

--- server/models/User.models.js
+++ server/models/User.models.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const Schema = new mongoose.Schema({
     name:{
         type:String,
@@ -17,6 +17,22 @@ const Schema = new mongoose.Schema({
     timestamps:true
 })
 
+Schema.pre("save", async function(next){
+    if(this.isModified("password")){
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+    }
+    next();
+})
+
+Schema.methods.comparePassword = async function(password){
+            const isMatch = await bcrypt.compare(password,this.password)
+            return isMatch;
+}
+
+
 const model = mongoose.model("user",Schema);
+
+
 
 module.exports= model;
